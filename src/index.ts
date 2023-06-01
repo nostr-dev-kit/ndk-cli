cb74a3204a100cff0482255b3b4b95774e0948e5
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-06-01 12:38:47 +0200

    bump NDK, redis-cache, and version

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
 
-import yargs, { boolean } from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import profile from './commands/profile';
-import follows from './commands/follows';
-import NDK, { NDKConstructorParams } from '@nostr-dev-kit/ndk';
-import NDKRedisCacheAdapter from '@nostr-dev-kit/ndk-cache-redis';
+import yargs, { boolean } from "yargs";
+import { hideBin } from "yargs/helpers";
+import profile from "./commands/profile";
+import follows from "./commands/follows";
+import NDK, { NDKConstructorParams } from "@nostr-dev-kit/ndk";
+import NDKRedisCacheAdapter from "@nostr-dev-kit/ndk-cache-redis";
 
-const defaultRelays = ['wss://nos.lol'];
+const defaultRelays = ["wss://nos.lol"];
 
-async function createNDK(
-    relays: string[],
-    cache: boolean
-): Promise<NDK> {
+async function createNDK(relays: string[], cache: boolean): Promise<NDK> {
     const ndkOpts: NDKConstructorParams = {};
 
     if (cache) {
@@ -30,34 +27,46 @@ async function createNDK(
 }
 
 yargs(hideBin(process.argv))
-    .command('profile <npub>', 'get profile data', () => {}, async (argv) => {
-        const relays = argv.relays || defaultRelays;
-        const cache = argv.redis || false;
-        const ndk = await createNDK(relays as string[], cache as boolean);
-        profile(ndk, {npub: argv.npub as string});
+    .command(
+        "profile <npub>",
+        "get profile data",
+        () => {},
+        async (argv) => {
+            const relays = argv.relays || defaultRelays;
+            const cache = argv.redis || false;
+            const ndk = await createNDK(relays as string[], cache as boolean);
+            profile(ndk, { npub: argv.npub as string });
+        }
+    )
+    .command(
+        "follows <npub>",
+        "get follows for an npub",
+        (yargs) => {
+            yargs.option("profile", {
+                alias: "p",
+                type: "boolean",
+                description: "Fetch profiles for follows",
+            });
+        },
+        async (argv) => {
+            const relays = argv.relays || defaultRelays;
+            const cache = argv.redis || false;
+            const fetchProfile = argv.profile || false;
+            const ndk = await createNDK(relays as string[], cache as boolean);
+            follows(ndk, {
+                npub: argv.npub as string,
+                fetchProfile: fetchProfile as boolean,
+            });
+        }
+    )
+    .option("relays", {
+        alias: "r",
+        type: "array",
+        description: "Relay URLs to use",
     })
-    .command('follows <npub>', 'get follows for an npub', (yargs) => {
-        yargs.option('profile', {
-            alias: 'p',
-            type: 'boolean',
-            description: 'Fetch profiles for follows'
-        });
-    }, async (argv) => {
-        const relays = argv.relays || defaultRelays;
-        const cache = argv.redis || false;
-        const fetchProfile = argv.profile || false;
-        const ndk = await createNDK(relays as string[], cache as boolean);
-        follows(ndk, {npub: argv.npub as string, fetchProfile: fetchProfile as boolean});
-    })
-    .option('relays', {
-        alias: 'r',
-        type: 'array',
-        description: 'Relay URLs to use'
-    })
-    .option('redis', {
-        type: 'boolean',
-        description: 'Use redis for caching'
+    .option("redis", {
+        type: "boolean",
+        description: "Use redis for caching",
     })
     .demandCommand(1)
     .parse();
-
